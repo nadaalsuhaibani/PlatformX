ca462834d14327891bc52a68c753ba47c0d2d032
lamaalturki <lalturki@site.sa>
2020-05-11 21:43:20 +0300

    Fix conflict

--- src/services/reducers.js
+++ src/services/reducers.js
@@ -6,20 +6,10 @@ import cartReducer from './cart/reducer';
 import totalReducer from './total/reducer';
 import filtersReducer from './filters/reducer';
 import sortReducer from './sort/reducer';
-
-<<<<<<< HEAD
-
-export const rootReducer = combineReducers({
-  firebase: firebaseReducer,
-  firestore: firestoreReducer,
-=======
 import authReducer from "./auth";
 import apiStatusReducer from "./apiStatus";
-import {firebaseReducer} from "react-redux-firebase";
-import {firestoreReducer} from "redux-firestore";
 
 export default combineReducers({
->>>>>>> 789e03cc0dd15c28520c222c09cc66004dad86cb
   shelf: shelfReducer,
   cart: cartReducer,
   total: totalReducer,
