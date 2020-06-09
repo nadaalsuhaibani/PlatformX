29e39828733b1728e4436cb55ec41a233ff0ef08
Lama Alturki <Lalturki@site.sa>
2020-06-09 16:18:59 +0300

    Add fetch products

--- src/components/Shelf/ProductList/index.js
+++ src/components/Shelf/ProductList/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
-
+import { useSelector } from 'react-redux';
+import { useFirestoreConnect, useFirestore } from 'react-redux-firebase';
 import Product from './Product';
+import PropTypes from 'prop-types';
+import Loading from '../../Loading'
 
-const ProductList = ({ products }) => {
-  return products.map(p => {
-    return <Product product={p} key={p.id} />;
-  });
+export default function ProductList() {
+  useFirestoreConnect([
+    { collection: "products" }
+  ]);
+  const products = useSelector(state => state.firestore.ordered.products);
+  console.log(products);
+
+
+  if (!products) return <Loading/>;
+
+  return products.map(p =>(
+    <Product product={p} key={p.id} />
+  ))
+
 };
-
-export default ProductList;
